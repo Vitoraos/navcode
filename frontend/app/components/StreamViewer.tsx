01b6443bcbae757dc233452f4a20977b16a8ed42
Vitora OS <emmykarl07@gmail.com>
2026-04-18 15:25:33 +0100

    Delete frontend/app/components directory

--- frontend/app/components/StreamViewer.tsx
+++
@@ -1,7 +0,0 @@
-export default function StreamViewer({ output }: any) {
-  return (
-    <div className="bg-zinc-900 p-3 h-60 overflow-auto">
-      <pre className="whitespace-pre-wrap">{output}</pre>
-    </div>
-  );
-}
